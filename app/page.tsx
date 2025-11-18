4316fcaba2e2139fa0848aa61ea349f7caf072ec
Olivierkepler <olivierkfrancois1@gmail.com>
2025-11-18 16:54:35 -0500

    update

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,7 @@ import ContactForm from './components/contact';
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h1>Hello World</h1>
-  <p>This is a test</p>
- <p>me</p>
+
   <ContactForm />
     </div>
   );
