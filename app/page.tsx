8ae705907fccf0db582bb522d825c82c2e16f36a
Olivierkepler <olivierkfrancois1@gmail.com>
2025-11-22 20:42:27 -0500

    updat

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ import PricingSection from "./components/PricingSection";
 import FeaturesSection from "./components/FeaturesSection";
 import TestimonialsSection from "./components/TestimonialsSection";
 import FAQSection from "./components/FAQSection";
-import Hero3D from "./components/Hero3D";
+
 export default function HomePage() {
   const [isConsultationOpen, setIsConsultationOpen] = useState(false);
 
@@ -19,10 +19,8 @@ export default function HomePage() {
 <div className="mt-16 sm:mt-20 md:mt-24">
 <Subnav />
 </div>
-      {/* <Hero openConsultation={() => setIsConsultationOpen(true)} /> */}
-     <div className="w-full max-w-full overflow-hidden fixed sm:static left-0 top-0 sm:relative">
-       <Hero3D openConsultation={() => setIsConsultationOpen(true)} />
-     </div>
+     
+     <Hero openConsultation={() => setIsConsultationOpen(true)} />
      
       <ServicesSection />
       <PricingSection openConsultation={() => setIsConsultationOpen(true)} />
