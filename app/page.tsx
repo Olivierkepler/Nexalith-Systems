33143d3ec7b6c2ff2caff4b52da1cd48be6e0a1c
Olivierkepler <olivierkfrancois1@gmail.com>
2025-11-22 21:11:38 -0500

    updat

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import Hero from "../app/components/Hero";
+import Hero from "../app/components/Hero"; // Correct path to Hero component
 import ConsultationModal from "./components/ConsultationForm";
 import Subnav from "./components/Subnav";
 import ServicesSection from "./components/ServicesSection";
@@ -14,31 +14,26 @@ export default function HomePage() {
   const [isConsultationOpen, setIsConsultationOpen] = useState(false);
 
   return (
-    <div className="min-h-screen w-full bg-zinc-50 dark:bg-black flex justify-center">
-      <main className="w-full max-w-full mx-auto  ">
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+   <main className="w-full max-w-full ">
+    
 
-        {/* Subnav */}
-        <div className="mt-16 sm:mt-20 md:mt-24 flex justify-center">
-          <Subnav />
-        </div>
-
-        {/* Hero */}
-        <Hero openConsultation={() => setIsConsultationOpen(true)} />
-
-        {/* Main Sections */}
-        <div className="flex flex-col items-center w-full">
-          <ServicesSection />
-          <PricingSection openConsultation={() => setIsConsultationOpen(true)} />
-          <FeaturesSection />
-          <TestimonialsSection />
-          <FAQSection />
-        </div>
-
-        {/* Modal */}
-        <ConsultationModal
-          isOpen={isConsultationOpen}
-          setIsOpen={setIsConsultationOpen}
-        />
+<div className="mt-16 sm:mt-20 md:mt-24">
+<Subnav />
+</div>
+     
+     <Hero openConsultation={() => setIsConsultationOpen(true)} />
+     
+      <ServicesSection />
+      <PricingSection openConsultation={() => setIsConsultationOpen(true)} />
+        <FeaturesSection />
+        <TestimonialsSection />
+        <FAQSection />
+        
+      <ConsultationModal
+        isOpen={isConsultationOpen}
+        setIsOpen={setIsConsultationOpen}
+      />
       </main>
     </div>
   );
