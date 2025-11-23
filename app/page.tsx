a26f12de733639580dddde60894164d7065cb7ef
Olivierkepler <olivierkfrancois1@gmail.com>
2025-11-22 21:19:02 -0500

    updat

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import Hero from "../app/components/Hero"; // Correct path to Hero component
-import ConsultationModal from "./components/ConsultationForm";
 import Subnav from "./components/Subnav";
-import ServicesSection from "./components/ServicesSection";
-import PricingSection from "./components/PricingSection";
-import FeaturesSection from "./components/FeaturesSection";
-import TestimonialsSection from "./components/TestimonialsSection";
-import FAQSection from "./components/FAQSection";
 
 export default function HomePage() {
   const [isConsultationOpen, setIsConsultationOpen] = useState(false);
@@ -22,18 +15,7 @@ export default function HomePage() {
 <Subnav />
 </div>
      
-     <Hero openConsultation={() => setIsConsultationOpen(true)} />
-     
-      <ServicesSection />
-      <PricingSection openConsultation={() => setIsConsultationOpen(true)} />
-        <FeaturesSection />
-        <TestimonialsSection />
-        <FAQSection />
-        
-      <ConsultationModal
-        isOpen={isConsultationOpen}
-        setIsOpen={setIsConsultationOpen}
-      />
+    
       </main>
     </div>
   );
