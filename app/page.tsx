1d0e42ae338b4b00f29140154f00651f15324c35
Olivierkepler <olivierkfrancois1@gmail.com>
2025-11-22 21:28:16 -0500

    updat

--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,18 @@
 
 import { useState } from "react";
 import Subnav from "./components/Subnav";
-
+import Hero from "./components/Hero";
 export default function HomePage() {
   const [isConsultationOpen, setIsConsultationOpen] = useState(false);
 
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      
    <main className="w-full max-w-full ">
-    
+   <Subnav />
+   <Hero openConsultation={() => setIsConsultationOpen(true)} />
 
-<div className="mt-16 sm:mt-20 md:mt-24">
-<Subnav />
-</div>
+   
      
     
       </main>
