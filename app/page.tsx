380915d2e42c0a84ac5085c3a7404a31f600e796
Olivierkepler <olivierkfrancois1@gmail.com>
2026-02-10 14:48:36 -0500

    update

--- app/page.tsx
+++ app/page.tsx
@@ -169,7 +169,7 @@ export default function HomePage() {
           <div className="absolute inset-0 p-6 flex flex-col justify-center gap-2">
             <h1 className="text-2xl md:text-4xl font-bold text-zinc-900 dark:text-white">
               {/* {content.hero.title} */}
-              hello
+              hello.
             </h1>
             <p className="mt-1 text-zinc-700 dark:text-zinc-300">
               {content.hero.subtitle}
